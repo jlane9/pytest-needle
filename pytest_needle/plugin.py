15981796d380484a09cbda54d9fd3a42bd2f8c5e
John Lane <jlane@sportinginnovations.com>
2017-06-02 23:46:13 -0500

    Add diff files to html report

--- pytest_needle/plugin.py
+++ pytest_needle/plugin.py
@@ -5,9 +5,12 @@
 """
 
 from __future__ import absolute_import
+import base64
+import os
 import pytest
 from pytest_needle.driver import DEFAULT_BASELINE_DIR, DEFAULT_OUTPUT_DIR, DEFAULT_ENGINE, \
     DEFAULT_VIEWPORT_SIZE, NeedleDriver
+from pytest_needle.exceptions import ImageMismatchException
 
 
 def pytest_addoption(parser):
@@ -38,6 +41,74 @@ def pytest_addoption(parser):
     group.addoption('--needle-viewport-size', action='store', dest='viewport_size',
                     metavar='pixels', default=DEFAULT_VIEWPORT_SIZE,
                     help='size of window width (px) x height (px)')
+
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item, call):
+    """Add image diff to report
+
+    :param item:
+    :param call:
+    :return:
+    """
+
+    outcome = yield
+    report = outcome.get_result()
+    report.extra = getattr(report, 'extra', [])
+
+    if is_failure(report) and call.excinfo:
+
+        exception = call.excinfo.value
+
+        if isinstance(exception, ImageMismatchException):
+
+            pytest_html = item.config.pluginmanager.getplugin('html')
+
+            if pytest_html is not None:
+
+                if os.path.exists(exception.baseline_image):
+
+                    report.extra.append(pytest_html.extras.image(
+                        get_image_as_base64(exception.baseline_image),
+                        'PDIFF: Expected'
+                    ))
+
+                diff_path = exception.output_image.replace('.png', '.diff.png')
+
+                if os.path.exists(diff_path):
+                    report.extra.append(pytest_html.extras.image(
+                        get_image_as_base64(diff_path),
+                        'PDIFF: Comparison'
+                    ))
+
+                if os.path.exists(exception.output_image):
+
+                    report.extra.append(pytest_html.extras.image(
+                        get_image_as_base64(exception.output_image),
+                        'PDIFF: Actual'
+                    ))
+
+
+def is_failure(report):
+    """True, if test failed
+
+    :param report:
+    :return:
+    """
+
+    xfail = hasattr(report, 'wasxfail')
+    return (report.skipped and xfail) or (report.failed and not xfail)
+
+
+def get_image_as_base64(filename):
+    """Open image from file as base64 encoded string
+
+    :param str filename: File path
+    :return:
+    """
+
+    with open(filename, 'rb') as image:
+        return base64.b64encode(image.read()).decode('ascii')
 
 
 @pytest.fixture()
