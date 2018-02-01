d667d1c8573b5962367614a6143af85d0c9fac91
John Lane <jlane@sportinginnovations.com>
2018-02-01 16:13:50 -0600

    Update baseline creation test to remove baseline image
    Since the travis CI build runs --save-baseline before running the actual test, this isn't really testing the functionality that was introduced. The baselines will always be present,  which means that this test is only testing that the fresh images are saved.

--- test/test_screenshot_creation.py
+++ test/test_screenshot_creation.py
@@ -13,8 +13,14 @@ def test_screenshot(needle):
 
     SCREENSHOT_NAME = 'screenshot_without_baseline'
     needle.driver.get('https://www.example.com')
+    baseline_dir = os.path.join(needle.baseline_dir, needle.build) if needle.categorize else needle.baseline_dir
+    baseline_path = os.path.join(baseline_dir, SCREENSHOT_NAME + ".png")
     screenshot_dir = os.path.join(needle.output_dir, needle.build) if needle.categorize else needle.output_dir
     screenshot_path = os.path.join(screenshot_dir, SCREENSHOT_NAME + ".png")
+
+    # Remove baseline image if exists
+    if os.path.exists(baseline_path):
+        os.remove(baseline_path)
 
     try:
         needle.assert_screenshot(SCREENSHOT_NAME)
